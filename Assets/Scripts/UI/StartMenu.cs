efb0782a1211e0ca330644b6d2be790cc61c1906
Ashley <ashleykeeling2020@gmail.com>
2026-04-30 13:39:43 +0100

    created sub menus on start menu for game mode and levels

--- Assets/Scripts/UI/StartMenu.cs
+++ Assets/Scripts/UI/StartMenu.cs
@@ -4,6 +4,37 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
+    public GameObject MainMenuPanel;
+    public GameObject PlayMenuPanel;
+    public GameObject LevelsMenuPanel;
+
+    private void Start()
+    {
+        ShowMainMenu();
+
+    }
+
+    public void ShowMainMenu()
+    {
+        MainMenuPanel.SetActive(true);
+        PlayMenuPanel.SetActive(false);
+        LevelsMenuPanel.SetActive(false);
+    }
+
+    public void ShowPlayMenu()
+    {
+        MainMenuPanel.SetActive(false);
+        PlayMenuPanel.SetActive(true);
+        LevelsMenuPanel.SetActive(false);
+    }
+
+    public void ShowLevelsMenu()
+    {
+        MainMenuPanel.SetActive(false);
+        PlayMenuPanel.SetActive(false);
+        LevelsMenuPanel.SetActive(true);
+    }
+
     public void PlayGame()
     {
         // run next scene
