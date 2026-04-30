3ef9a35b0b834ff41a646b22815769be8b2410b2
Ashley <ashleykeeling2020@gmail.com>
2026-04-30 12:37:55 +0100

    created singleton

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    public static GameSession Instance;
+    public enum GameMode
+    {
+        Levels,
+        Endless
+    }
+    public GameMode selectedMode;
+    public int levelIndex;
+
+
+    private void Awake()
+    {
+        // If another instance exists, destroy this one
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // Set this as the single instance
+        Instance = this;
+
+        // Make it persist between scenes
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SetLevelMode()
+    {
+        Instance.selectedMode = GameMode.Levels;
+    }
+
+    public void SetEndlessMode()
+    {
+        Instance.selectedMode = GameMode.Endless;
+    }
+
+    public void SetLevelMode(int index)
+    {
+        levelIndex = index;
+    }
+}
